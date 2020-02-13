def0b3cfd2ea5c2a1987e0a140bba36db10347cd
Dahlia <d.aljabari75@gmail.com>
2020-02-12 20:31:24 -0800

    Window mwssages with Api browser exercises 1-6

---
+++ JS/ex_123/index.js
@@ -0,0 +1,9 @@
+/* Define a students array and assign more than 6 student names
+Show the second and sixth students name in uppercase
+Also, show the students array length with the following format:
+The students array has 8 items*/
+let students = ['dahlia', 'sidratt', 'rizma', 'saba', 'afyaa', 'Ahmed', 'Zakariya'];
+
+
+console.log(students[1].toUpperCase(), students[5].toUpperCase());
+console.log(`The students array has ${students.length} items`);
---
+++ browserApi/ex_2/script.js
@@ -0,0 +1,8 @@
+//Using the console show odd numbers from 1 to 1000
+let num = 0;
+while (num < 1001) {
+    if ((num%2)) {
+        console.log(num);
+    }
+    num++;
+}
