2ce6d502cc1b01e5a585c8782f1a6ba0c12d8bfb
Dahlia <d.aljabari75@gmail.com>
2020-02-24 19:26:33 -0800

    add .gitignore file

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,11 +7,18 @@
 // console.log(lastname);
 // console.log(age);
 
+// const express = require('express')
+// const app = express()
+ 
+// app.get('/', function (req, res) {
+//   res.send('<h1>Hello World</h1>')
+// })
+ 
+// app.listen(3000)
+
 const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>')
-})
- 
-app.listen(3000)
+console.log(express)
+
+// const calc = require('./calculater')
+// let rsult = calc.sum(2, 3)
+// console.log(`Thee result is: ${rsult}`);
