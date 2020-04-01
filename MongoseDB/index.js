1e5826eae9d300e637ea88e3861000fb16b88ee3
Dahlia <d.aljabari75@gmail.com>
2020-03-31 22:27:53 -0700

    connect to Atlas DB

--- MongoseDB/index.js
+++ MongoseDB/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 //Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/comics';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+//const mongoDB = 'mongodb://localhost:27017/comics';
+const mongoDB = 'mongodb://Node-Example:Node-Example123@cluster0-shard-00-00-h0p3y.mongodb.net:27017,cluster0-shard-00-01-h0p3y.mongodb.net:27017,cluster0-shard-00-02-h0p3y.mongodb.net:27017/comics?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 //Mongoose Model (like Declare the Schemah)
 const Superheroe = mongoose.model('Superheroe', { 
   name: String,
